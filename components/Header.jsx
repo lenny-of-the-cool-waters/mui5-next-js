450ce4f43993baf355bf1697d2d63c320685e8ee
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-01-11 08:55:04 +0300

    Added navigation components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,15 @@
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Home from "@mui/icons-material/Home";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUp from "@mui/icons-material/KeyboardArrowUp";
+import BackToTop from "./BackToTop";
+import Navbar from "./Navbar";
+import SideDrawer from "./SideDrawer";
+import HideOnScroll from "./HideOnScroll";
+import MuiNextLink from "@components/MuiNextLink";
 import { styled } from "@mui/system";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
@@ -8,19 +17,45 @@ const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 const Header = () => {
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Container
-            maxWidth="lg"
-            sx={{ display: `flex`, justifyContent: `space-between` }}
-          >
-            This is a header
-          </Container>
-        </Toolbar>
-      </AppBar>
-      <Offset />
+      <HideOnScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Container
+              maxWidth="lg"
+              sx={{ display: `flex`, justifyContent: `space-between` }}
+            >
+              <IconButton edge="start" aria-label="home">
+                <MuiNextLink activeClassName="active" href="/">
+                  <Home
+                    sx={{
+                      color: (theme) => theme.palette.common.white,
+                    }}
+                    fontSize="large"
+                  />
+                </MuiNextLink>
+              </IconButton>
+              <Navbar navLinks={navLinks} />
+              <SideDrawer navLinks={navLinks} />
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Offset id="back-to-top-anchor" />
+      <BackToTop>
+        <Fab color="secondary" size="large" aria-label="back to top">
+          <KeyboardArrowUp />
+        </Fab>
+      </BackToTop>
     </>
   );
 };
 
 export default Header;
+
+export const navLinks = [
+  { title: `home`, path: `/` },
+  { title: `about us`, path: `/about-us` },
+  { title: `menu`, path: `/menu` },
+  { title: `catering`, path: `/catering` },
+  { title: `contact`, path: `/contact` },
+];
