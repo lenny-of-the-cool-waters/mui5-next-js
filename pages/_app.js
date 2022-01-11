28e5dd8a162c9f0de018856c5b4209f5fd3fa163
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-01-11 17:21:38 +0300

    Added Footer

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import createEmotionCache from '../styles/createEmotionCache';
 import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 import "/styles/globals.css";
 import theme from '../styles/theme';
@@ -26,6 +27,7 @@ export default function MyApp(props) {
         <CssBaseline />
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Hero from "@components/Hero";
 import SectionAbout from "@components/SectionAbout";
 import SectionImage from "@components/SectionImage";
 import SectionReview from "@components/SectionReview";
-
+import NavigationCard from "@components/NavigationCard";
 
 const Homepage = () => {
   return (
@@ -18,6 +19,33 @@ const Homepage = () => {
       <SectionAbout />
       <SectionImage imgSrc="/fusion-food.jpg" imgAlt="fusion food" />
       <SectionReview />
+
+      <SectionImage imgSrc="/sushi.jpg" imgAlt="fusion sushi" />
+
+      <Container maxWidth="md" sx={{ my: 15 }}>
+        <Grid container spacing={2}>
+          <Grid container item justifyContent="center" xs={12} md={6}>
+            <NavigationCard
+              imgSrc="/menu.jpg"
+              imgAlt="food menu"
+              title="Menu"
+              desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, minus."
+              pagePath="/menu"
+              ctaText="Check Out"
+            />
+          </Grid>
+          <Grid container item justifyContent="center" xs={12} md={6}>
+            <NavigationCard
+              imgSrc="/catering.jpg"
+              imgAlt="catering"
+              title="Catering"
+              desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, minus."
+              pagePath="/catering"
+              ctaText="Find Out"
+            />
+          </Grid>
+        </Grid>
+      </Container>
 
       {/* <Container maxWidth="sm">
         <h1>Home Page</h1>
